6b11bb547ca7308db55ea374f8703c54488abfc4
nimai <nimai.m.patel@gmail.com>
2021-12-12 17:12:06 +0530

    Added solution to Day 1 problem.

---
+++ day-1/main.hs
@@ -0,0 +1,10 @@
+getIncreases :: [Integer] -> Int
+getIncreases xs = length $ filter (LT ==) ordering
+  where
+    getOrdering (x1 : x2 : xs) = compare x1 x2 : getOrdering (x2 : xs)
+    getOrdering _ = []
+    ordering = getOrdering xs
+
+main = do
+  input <- map read . lines <$> readFile "input.txt"
+  print (getIncreases input)
